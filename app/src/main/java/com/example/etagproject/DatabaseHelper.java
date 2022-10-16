69c2bb1a6e7ce763cbb3d9db0c2f0adbb80e72e5
mayer <mayer.marcell7744@gmail.com>
2022-10-16 16:43:38 +0200

    sok update, very sok update

---
+++ app/src/main/java/com/example/etagproject/DatabaseHelper.java
@@ -0,0 +1,90 @@
+package com.example.etagproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "tag_table";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "name";
+    private static final String COL3 = "card_bar_code";
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, card_bar_code TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean addData(String itemTagName, String itemBarCode){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(itemTagName, itemBarCode);
+
+        Log.d(TAG, "addData: Adding " + itemTagName + " to " + TABLE_NAME);
+
+        long result = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String query = ("SELECT * FROM " + TABLE_NAME);
+        Cursor data = sqLiteDatabase.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getItemID(String name){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String query = "SELECT " + COL1 + " FROM " + TABLE_NAME + " WHERE " + COL2 + " = '" + name + "'";
+        Cursor data = sqLiteDatabase.rawQuery(query, null);
+        return data;
+    }
+
+    public void updateTag(String newName, int id,String oldName){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL2 +
+                " = '" + newName + "' WHERE " + COL1 + " = '" + id + "'" +
+                " AND " + COL2 + " = '" + oldName + "'";
+        Log.d(TAG, "updateName: query: " + query);
+        Log.d(TAG, "updateName: Setting name to " + newName);
+        sqLiteDatabase.execSQL(query);
+    }
+
+    public void deleteName(int id, String name){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL1 + " = '" + id + "'" +
+                " AND " + COL2 + " = '" + name + "'";
+        Log.d(TAG, "deleteName: query: " + query);
+        Log.d(TAG, "deleteName: Deleting " + name + " from database.");
+        sqLiteDatabase.execSQL(query);
+    }
+
+}
