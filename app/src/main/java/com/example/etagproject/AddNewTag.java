aec8e7a4db4f53bb1e1d4482d26b37cb34b32dcd
mayer <mayer.marcell7744@gmail.com>
2022-11-03 23:04:09 +0100

    camera autofocus fix

--- app/src/main/java/com/example/etagproject/AddNewTag.java
+++ app/src/main/java/com/example/etagproject/AddNewTag.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
 
 public class AddNewTag extends AppCompatActivity {
     private static final String TAG = "AddNewTag";
@@ -26,6 +28,7 @@ public class AddNewTag extends AppCompatActivity {
     private EditText setBarCode;
     private ImageButton scanWithCamera;
     MainActivity mainActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
