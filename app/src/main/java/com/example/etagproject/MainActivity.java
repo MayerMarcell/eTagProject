fb334d66e7bee8629bf8eab0ffdd985a5b7dcb03
mayer <mayer.marcell7744@gmail.com>
2022-11-06 21:43:03 +0100

    version fix

--- app/src/main/java/com/example/etagproject/MainActivity.java
+++ app/src/main/java/com/example/etagproject/MainActivity.java
@@ -198,9 +198,4 @@ public class MainActivity extends AppCompatActivity {
     private void toastMessage(String message){
         Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
-
-
-
-
-
 }
